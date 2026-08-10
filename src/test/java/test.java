4d1ac598b7675fa7234d36e731082eaaa7dfa1bd
mixplus <wuyouqitian@gmail.com>
2026-08-10 10:33:57 +0900

    feat: add DataUnit feat: add Percentage utility feat: add CPU monitor feat: add Memory monitor docs: add Memory Javadocs

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -1,12 +1,8 @@
-import com.mixplus.library.system.CPU;
-import com.mixplus.library.system.Memory;
-import unit.DataUnit;
-import unit.Percentage;
+
+import com.mixplus.library.*;
 
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
-
 
 public class test {
     private static final ScheduledExecutorService service =
