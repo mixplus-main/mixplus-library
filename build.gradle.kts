55d646ca7b184ff41172df1848da56f6eeeb0324
mixplus <wuyouqitian@gmail.com>
2026-09-23 06:25:48 +0900

    Add configurable HTTP request timeout
    - Allow Https.request() to accept a Duration timeout
    - Apply the timeout to HttpRequest

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "com.mixplus.library"
-version = "1.14.23-SNAPSHOT"
+version = "1.14.33-SNAPSHOT"
 
 publishing {
     repositories {
--- src/main/java/com/mixplus/library/network/Https.java
+++ src/main/java/com/mixplus/library/network/Https.java
@@ -4,11 +4,11 @@ import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
 import java.io.IOException;
-import java.io.InterruptedIOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.time.Duration;
 import java.util.Map;
 
 
@@ -23,9 +23,18 @@ public class Https {
     }
 
     public static Response request(String url) {
+        return request(url, Duration.ofSeconds(5));
+    }
+
+    public static Response request(String url, Duration timeout) {
+        return request(URI.create(url), timeout);
+    }
+
+    public static Response request(URI uri, Duration timeout) {
         try {
             HttpRequest request = HttpRequest.newBuilder()
-                    .uri(URI.create(url))
+                    .uri(uri)
+                    .timeout(timeout)
                     .GET()
                     .build();
 
